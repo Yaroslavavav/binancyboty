9240c66b1b1961bdbc02b61933fa6ccca2d9cce8
Yaroslav <yaroslavavav@Users-MacBook-Pro.local>
2023-06-30 17:29:03 +0200

    feat: create, delete logic

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import { makeAutoObservable } from 'mobx';
 import { observer } from 'mobx-react';
 import AgentTable from './components/Table';
 import { client } from './api/client';
+import { Button, message } from 'antd';
 // import styles from './index.module.scss'; // Import SCSS module styles
 
-class PostStore {
+class AgentStore {
   posts = [];
 
   constructor() {
@@ -20,15 +21,56 @@ class PostStore {
       console.error(error);
     }
   }
+
+  async createAgent(agent: any) {
+    try {
+      const response = await client.post<any>('/agent/spawn', agent);
+      return response;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async deleteAgent(agentId: string) {
+    try {
+      const response = await client.post<any>(`/agent/kill/${agentId}`);
+      return response;
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
 
-const postStore = new PostStore();
+export const agentStore = new AgentStore();
+
+const testAgent = {
+  investment: 42,
+  updatesPerInterval: 421,
+  setId: {
+    symbol: 'SOLUSDT',
+    interval: 3,
+  },
+  takeProfit: 0.4,
+  stopLoss: 0.1,
+  methodConfig: {
+    MethodName: 'macd-rsi',
+    MethodConfigProperties: {
+      Properties: {
+        MinGrowthIntervals: 2.6,
+        MacdSignalScale: 1.8,
+        MinRsiModification: 1.9,
+        MaxRsiModification: 2.5,
+        SignalThreshold: 0.6,
+      },
+    },
+  },
+};
 
 const App = observer(() => {
   const [agents, setAgents] = React.useState<any[]>([]);
 
   const fetchAgents = async () => {
-    const resp = await postStore.getAgents();
+    const resp = await agentStore.getAgents();
 
     setAgents(resp?.data);
   };
@@ -40,7 +82,27 @@ const App = observer(() => {
   return (
     <div>
       <h1>Hello, Sanyek! {`>:D`}</h1>
-      <AgentTable data={agents} />
+      <Button
+        type="primary"
+        onClick={async () => {
+          const min = 10;
+          const max = 100;
+          const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+          try {
+            const data = await agentStore.createAgent({
+              ...testAgent,
+              investment: randomNum,
+            });
+
+            await fetchAgents();
+          } catch (error) {
+            message.error('Oops, ne ne ne');
+          }
+        }}
+      >
+        Create
+      </Button>
+      <AgentTable data={agents} refetchAgents={() => fetchAgents()} />
     </div>
   );
 });
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,8 +1,12 @@
+import { Button, message } from 'antd';
 import Table, { ColumnsType } from 'antd/es/table';
 import React from 'react';
+import { agentStore } from '../App';
 
-const AgentTable: React.FC<{ data: any[] }> = ({ data }) => {
-  console.log(data);
+const AgentTable: React.FC<{ data: any[]; refetchAgents: () => void }> = ({
+  data,
+  refetchAgents,
+}) => {
   const columns: ColumnsType<any> = [
     {
       title: 'Agent ID',
@@ -38,15 +42,46 @@ const AgentTable: React.FC<{ data: any[] }> = ({ data }) => {
       title: 'Logs',
       dataIndex: 'logs',
       key: 'logs',
+      width: '30%',
       render: (logs: any[]) => {
         return <div>{logs.join(', ')}</div>;
+      },
+    },
+    {
+      title: '',
+      dataIndex: '',
+      key: '',
+      width: '5%',
+      render: (_: any, record: any) => {
+        return (
+          <Button
+            type="primary"
+            style={{ backgroundColor: 'tomato' }}
+            onClick={async () => {
+              try {
+                const data = await agentStore.deleteAgent(record.agentId);
+                console.log(data);
+
+                await refetchAgents();
+              } catch (error) {
+                message.error('Ne mojna');
+              }
+            }}
+          >
+            Delete
+          </Button>
+        );
       },
     },
   ];
 
   return (
     <div>
-      <Table dataSource={data} columns={columns} />
+      <Table
+        dataSource={data}
+        rowKey={(item) => item.agentId}
+        columns={columns}
+      />
     </div>
   );
 };
